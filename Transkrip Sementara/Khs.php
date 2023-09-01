c7dd2160c0cd6e9153adc7a5e27574d1097ae7f5
rakyankrisna <rakyankrisna@gmail.com>
2023-09-01 14:39:28 +0700

    init Transkrip Sementara

---
+++ Transkrip Sementara/Khs.php
@@ -0,0 +1,282 @@
+<?php
+//include
+include('header.php');
+include('body.php');
+include('footer.php');
+?>
+
+<?php
+//settings
+$tb_width = 800;
+$temp_head = 0;
+$temp_body = 0;
+$temp_footer = 0;
+?>
+
+<!-- Style Utama -->
+<style>
+    :root {
+        --font-utama: Arial, Helvetica, sans-serif;
+    }
+
+    p {
+        margin: 0;
+    }
+
+    .container {
+        font-family: var(--font-utama);
+        width: <?= $tb_width ?>;
+        border: 1px solid;
+        padding: 10px;
+        height: fit-content;
+    }
+
+
+    .main {
+        display: flex;
+        justify-content: center;
+        gap: 30px;
+        align-items: center;
+    }
+
+    @media print {
+
+
+        .input_header {
+            display: none;
+        }
+
+        .main{
+            width: 100%;
+        }
+
+        .container {
+            border: none;
+            width: 100%
+        }
+
+        .hide {
+            display: none;
+        }
+    }
+</style>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Custom Report</title>
+    <link rel="stylesheet" type="text/css" href="../Prism/prism.css">
+    <script type="text/javascript" src="../Prism/prism.js"></script>
+</head>
+
+<body>
+    <form action="#" method="POST">
+        <!-- input header -->
+        <div class="input_header">
+            <p>Header</p>
+            <input type="radio" name="header" value="1" <?php if (isset($_POST['header']) && $_POST['header'] === '1') echo 'checked'; ?>>1<br>
+            <input type="radio" name="header" value="2" <?php if (isset($_POST['header']) && $_POST['header'] === '2') echo 'checked'; ?>>2<br>
+            <input type="radio" name="header" value="3" <?php if (isset($_POST['header']) && $_POST['header'] === '3') echo 'checked'; ?>>3<br>
+            <input type="radio" name="header" value="4" <?php if (isset($_POST['header']) && $_POST['header'] === '4') echo 'checked'; ?>>4<br>
+            <input type="radio" name="header" value="5" <?php if (isset($_POST['header']) && $_POST['header'] === '5') echo 'checked'; ?>>5<br>
+            <!-- input body -->
+            <p>Body</p>
+            <input type="radio" name="body" value="1" <?php if (isset($_POST['body']) && $_POST['body'] === '1') echo 'checked'; ?>>1<br>
+            <input type="radio" name="body" value="2" <?php if (isset($_POST['body']) && $_POST['body'] === '2') echo 'checked'; ?>>2<br>
+            <input type="radio" name="body" value="3" <?php if (isset($_POST['body']) && $_POST['body'] === '3') echo 'checked'; ?>>3<br>
+            <input type="radio" name="body" value="4" <?php if (isset($_POST['body']) && $_POST['body'] === '4') echo 'checked'; ?>>4<br>
+            <input type="radio" name="body" value="5" <?php if (isset($_POST['body']) && $_POST['body'] === '5') echo 'checked'; ?>>5<br>
+            <!-- input footer -->
+            <p>Footer</p>
+            <input type="radio" name="footer" value="1" <?php if (isset($_POST['footer']) && $_POST['footer'] === '1') echo 'checked'; ?>>1<br>
+            <input type="radio" name="footer" value="2" <?php if (isset($_POST['footer']) && $_POST['footer'] === '2') echo 'checked'; ?>>2<br>
+            <input type="radio" name="footer" value="3" <?php if (isset($_POST['footer']) && $_POST['footer'] === '3') echo 'checked'; ?>>3<br>
+            <input type="radio" name="footer" value="4" <?php if (isset($_POST['footer']) && $_POST['footer'] === '4') echo 'checked'; ?>>4<br>
+            <input type="radio" name="footer" value="5" <?php if (isset($_POST['footer']) && $_POST['footer'] === '5') echo 'checked'; ?>>5<br>
+            <!-- submit -->
+            <input type="submit" value="Submit">
+        </div>
+    </form>
+
+    <!-- buka utama     -->
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    ?>
+        <div class="main">
+            <div class="container">
+                <?php
+                //header
+                if (isset($_POST['header'])) {
+                    $head = $_POST['header'];
+                    $temp_head = $head;
+                ?>
+
+                    <!-- header 1 -->
+                    <?php
+                    if ($head == 1) :
+                        echo $header1;
+                    endif;
+                    ?>
+                    <!-- header 2 -->
+                    <?php
+                    if ($head == 2) :
+                        echo $header2;
+                    endif;
+                    ?>
+                    <!-- header 3 -->
+                    <?php
+                    if ($head == 3) :
+                        echo $header3;
+                    endif;
+                    ?>
+                    <!-- header 4 -->
+                    <?php
+                    if ($head == 4) :
+                        echo $header4;
+                    endif;
+                    ?>
+                    <!-- header 5 -->
+                    <?php
+                    if ($head == 5) :
+                        echo $header5;
+                    endif;
+                    ?>
+
+
+                <?php
+                }
+                ?>
+
+                <?php
+                if (isset($_POST['body'])) {
+                    $body = $_POST['body'];
+                    $temp_body = $body;
+                ?>
+                    <!-- body 1 -->
+                    <?php
+                    if ($body == 1) :
+                        echo $body1;
+                    endif;
+                    ?>
+                    <!-- body 2 -->
+                    <?php
+                    if ($body == 2) :
+                        echo $body2;
+                    endif;
+                    ?>
+                    <!-- body 3 -->
+                    <?php
+                    if ($body == 3) :
+                        echo $body3;
+                    endif;
+                    ?>
+                    <!-- body 4 -->
+                    <?php
+                    if ($body == 4) :
+                        echo $body4;
+                    endif;
+                    ?>
+                    <!-- body 5 -->
+                    <?php
+                    if ($body == 5) :
+                        echo $body5;
+                    endif;
+                    ?>
+
+                <?php
+                }
+                ?>
+
+                <?php
+                if (isset($_POST['footer'])) {
+                    $footer = $_POST['footer'];
+                    $temp_footer = $footer;
+                ?>
+                    <!-- footer 1 -->
+                    <?php
+                    if ($footer == 1) :
+                        echo $footer1;
+                    endif;
+                    ?>
+                    <!-- footer 2 -->
+                    <?php
+                    if ($footer == 2) :
+                        echo $footer2;
+                    endif;
+                    ?>
+                    <!-- footer 3 -->
+                    <?php
+                    if ($footer == 3) :
+                        echo $footer3;
+                    endif;
+                    ?>
+                    <!-- footer 4 -->
+                    <?php
+                    if ($footer == 4) :
+                        echo $footer4;
+                    endif;
+                    ?>
+                    <!-- footer 5 -->
+                    <?php
+                    if ($footer == 5) :
+                        echo $footer5;
+                    endif;
+                    ?>
+
+                <?php
+                }
+                ?>
+            </div>
+            <!-- tutup utama     -->
+            <div class="container hide" style="background-color: black; overflow-y: scroll; max-height: 600px;">
+                <?php
+                //
+                echo '<pre><code class="language-html">' . htmlspecialchars("<div style='margin-top: 20px; font-family: 'Times New Roman', Times, serif; width: 800; margin: 0 auto;border: 1px solid;padding: 10px;'>");
+                //head
+                if ($temp_head == 1) {
+                    echo htmlspecialchars($header1);
+                } elseif ($temp_head == 2) {
+                    echo htmlspecialchars($header2);
+                } elseif ($temp_head == 3) {
+                    echo htmlspecialchars($header3);
+                } elseif ($temp_head == 4) {
+                    echo htmlspecialchars($header4);
+                } else {
+                    echo htmlspecialchars($header5);
+                }
+                //body
+                if ($temp_body == 1) {
+                    echo htmlspecialchars($body1);
+                } elseif ($temp_body == 2) {
+                    echo htmlspecialchars($body2);
+                } elseif ($temp_body == 3) {
+                    echo htmlspecialchars($body3);
+                } elseif ($temp_body == 4) {
+                    echo htmlspecialchars($body4);
+                } else {
+                    echo htmlspecialchars($body5);
+                }
+                //footer
+                if ($temp_footer == 1) {
+                    echo htmlspecialchars($footer1);
+                } elseif ($temp_footer == 2) {
+                    echo htmlspecialchars($footer2);
+                } elseif ($temp_footer == 3) {
+                    echo htmlspecialchars($footer3);
+                } elseif ($temp_footer == 4) {
+                    echo htmlspecialchars($footer4);
+                } else {
+                    echo htmlspecialchars($footer5);
+                }
+
+                echo htmlspecialchars("</div>") . '</pre></code>';
+                ?>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
+</body>
+
+</html>
---
+++ Transkrip Sementara/body.php
@@ -0,0 +1,399 @@
+<?php
+
+$htmlBody1 = '';
+for ($i = 1; $i < 8; $i++) {
+    $htmlBody1 .= "
+        <tr>
+            <td style='border: 1px solid; padding:4px;' align='center'>$i</td>
+            <td style='border: 1px solid; padding:4px;' align='left'>Fisika Rekayasa</td>
+            <td style='border: 1px solid; padding:4px;' align='left'>312312</td>
+            <td style='border: 1px solid; padding:4px;' align='center'>3</td>
+            <td style='border: 1px solid; padding:4px;' align='center'>A</td>
+            <td style='border: 1px solid; padding:4px;' align='center'>4.00</td>
+            <td style='border: 1px solid; padding:4px;' align='center'>12</td>
+            <td style='border: 1px solid; padding:4px;' align='center'>LULUS</td>
+        </tr>
+    ";
+}
+
+$htmlBody2 = '';
+for ($i = 1; $i < 8; $i++) {
+    $htmlBody2 .= "
+    <tr>
+        <td style='border: 1px solid; padding:4px;' align='center'>$i</td>
+        <td style='border: 1px solid; padding:4px;' align='left'>Fisika Rekayasa</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>2</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>2.00</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>A</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>4</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>2</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>2</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>LULUS</td>
+    </tr>
+    ";
+}
+
+
+$htmlBody3 = '';
+for ($i = 1; $i < 8; $i++) {
+    $htmlBody3 .= "
+        <tr>
+            <td width='40px'>$i</td>
+            <td width='90px'>312312</td>
+            <td width=''>Fisika Rekayasa</td>
+            <td width='50px'>3</td>
+            <td width=''>4.00</td>
+        </tr>
+    ";
+}
+
+
+$htmlBody4 = '';
+for ($i = 1; $i < 7; $i++) {
+    $htmlBody4 .= "
+    <tr>
+        <td style='border: 1px solid; padding:4px;' align='center'>$i</td>
+        <td style='border: 1px solid; padding:4px;' align='left'>312312</td>
+        <td style='border: 1px solid; padding:4px;' align='left'>Fisika Rekayasa</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>3</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>A</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>6.9</td>
+    </tr>
+    ";
+}
+
+
+$htmlBody5 = '';
+for ($i = 1; $i < 8; $i++) {
+    $htmlBody5 .= "
+    <tr>
+        <td style='border: 1px solid; padding:4px;' align='center'>$i</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>312312</td>
+        <td style='border: 1px solid; padding:4px;' align='left'>Fisika Rekayasa</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>3</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>A</td>
+        <td style='border: 1px solid; padding:4px;' align='center'>6.9</td>
+    </tr>
+    ";
+}
+
+$body1 = " 
+<div style='margin-top: 5px;'>
+    <table style='border-collapse: collapse; font-size: 14px;' width='100%'>
+        <thead>
+            <tr>
+                <th style='border: 1px solid;' width='40px' rowspan='2'>No.</th>
+                <th style='border: 1px solid;' width='' rowspan='2'>NAMA MATA KULIAH</th>
+                <th style='border: 1px solid;' width='90px' rowspan='2'>KODE</th>
+                <th style='border: 1px solid;' width='50px' rowspan='2'>SKS</th>
+                <th style='border: 1px solid;' width='' colspan='3'>NILAI AKHIR</th>
+                <th style='border: 1px solid;' width='140px' rowspan='2'>Ket</th>
+            </tr>
+            <tr>
+                <th style='border: 1px solid;' width='50px'>HM</th>
+                <th style='border: 1px solid;' width='50px'>NM</th>
+                <th style='border: 1px solid;' width='50px'>KN</th>
+            </tr>
+        </thead>
+        <tbody>
+            $htmlBody1
+            <tr>
+                <td style='border: 1px solid;' colspan='3' align='center'>JUMLAH</td>
+                <td style='border: 1px solid;' align='center'>20</td>
+                <td style='border: 1px solid;' align='center'></td>
+                <td style='border: 1px solid;' align='center'></td>
+                <td style='border: 1px solid;' align='center'>12</td>
+                <td style='border: 1px solid;' align='center'></td>
+            </tr>
+        </tbody>
+    </table>
+    <table style='border-collapse: collapse; font-size: 12px; margin-top:15px' width='100%'>
+        <tbody>
+        <tr>
+            <td width='260px' style='font-weight: bold; padding:4px;'>Indeks Prestasi</td>
+            <td width='260px' style='font-weight: bold; padding:4px;'>: 2.60</td>
+            <td width=''></td>
+        </tr>
+        <tr>
+            <td style='font-weight: bold; padding:4px;'>Beban SKS Maks Sem. yang akan Datang</td>
+            <td width='' style='font-weight: bold; padding:4px;'>: 24</td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+</div>";
+
+$body2 = "
+<div style='margin-top: 15px;'>
+    <table style='border-collapse: collapse; font-size: 14px;' width='100%'>
+        <thead>
+            <tr>
+                <th style='border: 1px solid;' width='40px' rowspan='2'>NO</th>
+                <th style='border: 1px solid;' width='' rowspan='2'>MATA KULIAH</th>
+                <th style='border: 1px solid;' width='60px' rowspan='2'>SKS</th>
+                <th style='border: 1px solid;' width='' colspan='2'>NILAI</th>
+                <th style='border: 1px solid;' width='60px' rowspan='2'>SKSN</th>
+                <th style='border: 1px solid;' width='60px' rowspan='2'>SKSD</th>
+                <th style='border: 1px solid;' width='70px' rowspan='2'>Rencana Ke</th>
+                <th style='border: 1px solid;' width='80px' rowspan='2'>Ket.</th>
+            </tr>
+            <tr>
+                <th style='border: 1px solid;' width='60px'>Angka</th>
+                <th style='border: 1px solid;' width='60px'>Huruf</th>
+            </tr>
+        </thead>
+        <tbody>
+            $htmlBody2
+        </tbody>
+    </table>
+    <table width='100%' style='text-align: center; margin-top: 15px; border-collapse: collapse; font-size: 14px;'>
+        <thead>
+            <tr>
+                <th width='' style='border: 1px solid; text-align: left; padding:4px;'>Jumlah</th>
+                <th width='100px' style='border: 1px solid;'>SKS</th>
+                <th width='100px' style='border: 1px solid;'>SKS Dicapai</th>
+                <th width='100px' style='border: 1px solid;'>SKS X Nilai</th>
+                <th width='100px' style='border: 1px solid;'>IP</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td style='border: 1px solid;  text-align: left; padding:4px;'>Semester Ini</td>
+                <td style='border: 1px solid;'>19</td>
+                <td style='border: 1px solid;'>17</td>
+                <td style='border: 1px solid;'>49</td>
+                <td style='border: 1px solid;'>2.58</td>
+            </tr>
+            <tr>
+                <td style='border: 1px solid;  text-align: left; padding:4px;'>Kumulatif</td>
+                <td style='border: 1px solid;'>141</td>
+                <td style='border: 1px solid;'>131</td>
+                <td style='border: 1px solid;'>395</td>
+                <td style='border: 1px solid;'>2.90</td>
+            </tr>
+        </tbody>
+    </table>
+    <table width='100%' style='margin-top: 25px; border-collapse: collapse; font-size: 14px;'>
+        <tr>
+            <td></td>
+            <td style=' font-weight: bold;'>Keterangan:</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>SKS</td>
+            <td>:</td>
+            <td>Satuan Kredit Semester</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>SKSN</td>
+            <td>:</td>
+            <td>SKS x Nilai</td>
+        </tr>
+        <tr>
+            <td width='70px'></td>
+            <td width='190px'>SKSD</td>
+            <td width=''>:</td>
+            <td width=''>SKS yang dicapai</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>SKS Semester</td>
+            <td>:</td>
+            <td>SKS yang dicapai semester ini</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>SKS Kumulatif</td>
+            <td>:</td>
+            <td>Total SKS yang Dicapai</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>IP Semester</td>
+            <td>:</td>
+            <td>Indeks Prestasi Semester</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>IP Kumulatif</td>
+            <td>:</td>
+            <td>Indeks Prestasi Kumulatif</td>
+        </tr>
+    </table>
+</div>";
+
+
+$body3 = "
+<div style='margin-top: 15px;'>
+    <table style='border-collapse: collapse; font-size: 14px; text-align: center;' width='100%'>
+        <thead style='border-top: 1px solid; border-bottom: 1px solid;'>
+            <tr>
+                <th width='40px'>NO.</th>
+                <th width='90px'>KODE MK</th>
+                <th width=''>NAMA MATA KULIAH</th>
+                <th width='50px'>SKS</th>
+                <th width=''>NILAI</th>
+            </tr>
+        </thead>
+        <tbody style='border-top: 1px solid; border-bottom: 1px solid;'>
+            $htmlBody3
+        </tbody>
+    </table>
+    <table style='border-collapse: collapse; font-size: 12px; margin-top:15px' width='100%'>
+        <tbody>
+        <tr>
+            <td width='240px' style='font-weight: bold;'>Indeks Prestasi (IP)</td>
+            <td width='210px' style='font-weight: bold;'>: 2.50</td>
+            <td width='' style='text-align: center;'></td>
+        </tr>
+        <tr>
+        <td style='font-weight: bold;'>Jumlah SKS</td>
+        <td width='' style='font-weight: bold;'>: 21</td>
+        <td style='text-align: center;'></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+";
+
+
+$body4 = "
+<div style='margin-top: 15px;'>
+    <table style='border-collapse: collapse; font-size: 12px; text-align: center;' width='100%'>
+        <thead style='border-top: 1px solid; border-bottom: 1px solid;'>
+            <tr>
+                <th style='border: 1px solid;' width='40px'>No.</th>
+                <th style='border: 1px solid;' width='90px'>Code</th>
+                <th style='border: 1px solid;' width=''>Subject</th>
+                <th style='border: 1px solid;' width='80px'>Credit (C)</th>
+                <th style='border: 1px solid;' width='80px'>Grade (G)</th>
+                <th style='border: 1px solid;' width='40px'>C.G</th>
+            </tr>
+        </thead>
+        <tbody style='border-top: 1px solid; border-bottom: 1px solid;'>
+            $htmlBody4
+            <tr>
+                <td style='border: 1px solid;' align='center' colspan='3'>Jumlah</td>
+                <td style='border: 1px solid;' align='center'></td>
+                <td style='border: 1px solid;' align='center'></td>
+                <td style='border: 1px solid;' align='center'></td>
+            </tr>
+        </tbody>
+    </table>
+    <table style='margin-top: 15px; border-collapse: collapse; font-size: 12px;' width='100%'>
+        <tbody>
+            <tr>
+                <td width='250px' align='left'>&nbsp;</td>
+                <td width='' align='left'>&nbsp;</td>
+                <td width='' style='border: 1px solid;' align='center'>Grade Point Average</td>
+                <td width='' style='border: 1px solid;' align='center'>Total Credits</td>
+                <td width='' style='border: 1px solid;' align='center'>Total Pass Credits</td>
+                <td width='' style='border: 1px solid;' align='center'>C.G</td>
+            </tr>
+            <tr>
+                <td align='left'>&nbsp;</td>
+                <td align='left'>General Average</td>
+                <td style='border: 1px solid;' align='center'>3.64</td>
+                <td style='border: 1px solid;' align='center'>22</td>
+                <td style='border: 1px solid;' align='center'>22</td>
+                <td style='border: 1px solid;' align='center'>80</td>
+            </tr>
+            <tr>
+                <td align='left'>&nbsp;</td>
+                <td align='left'>Grade Point Average</td>
+                <td style='border: 1px solid;' align='center'>3.73</td>
+                <td style='border: 1px solid;' align='center'>90</td>
+                <td style='border: 1px solid;' align='center'>90</td>
+                <td style='border: 1px solid;' align='center'>335.80</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+";
+
+$body5 = "
+<div style='margin-top: 15px;'>
+<table style='border-collapse: collapse; font-size: 12px; text-align: center;' width='100%'>
+    <thead style='border-top: 1px solid; border-bottom: 1px solid;'>
+        <tr>
+            <th style='border: 1px solid;' width='40px'>No.</th>
+            <th style='border: 1px solid;' width='50px'>Course Code</th>
+            <th style='border: 1px solid;' width=''>Course Name</th>
+            <th style='border: 1px solid;' width='80px'>Credit Unit</th>
+            <th style='border: 1px solid;' width='80px'>Grade</th>
+            <th style='border: 1px solid;' width='170px'>Weighted Score</th>
+        </tr>
+    </thead>
+    <tbody style='border-top: 1px solid; border-bottom: 1px solid;'>
+        $htmlBody5
+        <tr>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+        </tr>
+        <tr>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+        </tr>
+        <tr>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center' colspan='2'>Credit Units for this Semester</td>
+            <td style='border: 1px solid;' align='center'>19</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+        </tr>
+        <tr>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center' colspan='2'>Grade Point Average for this Semester</td>
+            <td style='border: 1px solid;' align='center'>2.46</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+        </tr>
+        <tr>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center' colspan='2'>Credit Units Cumulative</td>
+            <td style='border: 1px solid;' align='center'>37</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+        </tr>
+        <tr>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center' colspan='2'>Grade Point Average Cumulative</td>
+            <td style='border: 1px solid;' align='center'>2.79</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+            <td style='border: 1px solid;' align='center'>&nbsp;</td>
+        </tr>
+
+    </tbody>
+</table>
+<table style='border-collapse: collapse; font-size: 12px; margin-top:15px' width='100%'>
+    <tbody>
+        <tr>
+            <td width='240px' style='font-weight: bold;'>Cc:</td>
+            <td width='210px'></td>
+            <td width='' style='text-align: center;'></td>
+        </tr>
+        <tr>
+            <td style='font-weight: bold;'>1. Student</td>
+            <td width=''></td>
+            <td width=''></td>
+        </tr>
+        <tr>
+            <td style='font-weight: bold;'>2. Academic Operations</td>
+            <td width=''></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+</div>
+";
